c35af69fe355259c1f029d34b9568a150cb63ed5
Gian Sarinas <gsarinas17@gmail.com>
2024-10-31 20:10:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("animation");
+      } else {
+        $(tag).removeClass("animation");
+      }
+    }
+  });
+
+  const swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
